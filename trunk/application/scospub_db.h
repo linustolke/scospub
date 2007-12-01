64c2cc179b7680370ba5da7bfe63153e0a9f3e57
ltolke <ltolke@35392d16-733d-0410-b510-1166c866a665>
2007-12-01 16:13:50 +0000

    Reworked the work generator to work from settings in the database.

--- trunk/application/scospub_db.h
+++ trunk/application/scospub_db.h
@@ -15,13 +15,20 @@ struct SCOS_SOURCE {
     int project;
     int type;
 
+    // Static data
     // Subversion
     char url[256];
+    char username[35];
+    char password[35];
+
     char rooturl[256];
     char uuid[100];
     
     // CVS:
     // Other...
+
+    // Dynamic data
+    int lastrevision;
 
     int active;
 
@@ -73,8 +80,8 @@ struct SCOS_RESULT_SOURCE {
     class DB_SCOS_ ## type : public DB_BASE, public SCOS_ ## type { \
     public: \
 	DB_SCOS_ ## type(DB_CONN* p = 0); \
-	int get_id() const; \
-	void db_print(char*) const; \
+	int get_id(); \
+	void db_print(char*); \
 	void db_parse(MYSQL_ROW &row); \
     }
 
