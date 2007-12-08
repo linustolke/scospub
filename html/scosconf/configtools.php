31eb205a78718a94594249659079979d7fa334ce
ltolke <ltolke@35392d16-733d-0410-b510-1166c866a665>
2007-12-08 08:40:50 +0000

    First version of tool configuring page. Template tools in project 1. Not yet possible to configure.

---
+++ html/scosconf/configtools.php
@@ -0,0 +1,98 @@
+<?php
+
+require_once("../inc/db.inc");
+require_once("../inc/util.inc");
+require_once("../inc/translation.inc");
+
+init_session();
+db_init();
+
+page_head(tr(SCOSC_TITLE));
+
+$projid=$_REQUEST["project"];
+
+$result = mysql_query("SELECT * FROM scos_project WHERE id=$projid");
+$proj = mysql_fetch_object($result);
+mysql_free_result($result);
+
+echo tr(SCOSC_CONFIGURE_TOOLS_DESCRIPTION)."<p>
+$proj->name ($proj->user_friendly_name):";
+
+// Lets put the current configuration in an array.
+$result = mysql_query("SELECT * FROM scos_tool WHERE project=$projid");
+$enabled_tools = array();
+$enabled_tools_config = array();
+while ($tool = mysql_fetch_object($result)) {
+    $enabled_tools[$tool->name] = 1;
+
+    // 2 if active, 1 if not active
+    $enabled_tools_config[$tool->name.", ".$tool->config] = 1 + $tool->active;
+}
+mysql_free_result($result);
+
+echo "<UL>
+";
+
+// Now go through the template and generate a page.
+$result = mysql_query("SELECT name FROM scos_tool WHERE project=1 AND active=1 GROUP BY name");
+while ($tool = mysql_fetch_object($result)) {
+    echo "<LI>$tool->name:
+        <UL>
+            <LI><INPUT type='radio' name='$tool->name' value='off' ";
+    if ($enabled_tools[$tool->name] == 0) {
+        echo 'checked="true"';
+    }
+    echo ">".tr(SCOSC_TOOL_NOT_ENABLED)."</LI>
+    ";
+    $configresult = mysql_query("SELECT * FROM scos_tool WHERE project=1 AND active=1 AND name='$tool->name'");
+    while ($config = mysql_fetch_object($configresult)) {
+	echo "<LI><INPUT type='radio' name='$tool->name' value='$config->config' ";
+        if ($enabled_tools_config[$config->name.", ".$config->config] != 0) {
+            echo 'checked="1"';
+        }
+        echo ">$config->config</LI>
+        ";
+    }
+    mysql_free_result($configresult);
+
+    $configresult = mysql_query("SELECT * FROM scos_tool "
+	. "WHERE project=$projid "
+	. "AND name='$tool->name' "
+	. "AND config NOT IN (SELECT config FROM scos_tool WHERE name='$tool->name' AND project=1)");
+    if (mysql_numrows($configresult) > 0) {
+        echo tr(SCOSC_TOOLS_CONFIG_NOT_AVAILABLE).":<UL>
+        ";
+        while ($config->mysql_fetch_object($configresult)) {
+            echo "<LI>$config->name $config->config</LI>
+            ";
+        }
+        echo "</UL>
+        ";
+    }
+    mysql_free_result($configresult);
+
+    echo "</UL>
+    </LI>
+    ";
+}
+mysql_free_result($result);
+
+echo "</UL>
+";
+
+
+$result = mysql_query("SELECT * FROM scos_tool WHERE project=$projid AND name NOT IN (SELECT name FROM scos_tool WHERE project=1)");
+if (mysql_numrows($result) > 0) {
+    echo tr(SCOSC_TOOLS_NOT_AVAILABLE).":<UL>
+    ";
+    while ($tool->mysql_fetch_object($result)) {
+        echo "<LI>$tool->name $tool->config</LI>
+	";
+    }
+    echo "</UL>
+    ";
+}
+mysql_free_result($result);
+
+page_tail();
+?>
--- html/scosconf/projects.php
+++ html/scosconf/projects.php
@@ -11,132 +11,149 @@ $xml = $_GET['xml'];
 if ($xml) {
     require_once('../inc/xml.inc');
     xml_header();
-    echo "<scos_projects>\n";
 } else {
     page_head(tr(SCOSC_TITLE));
     echo tr(SCOSC_DESCRIPTION)."<p>
     ";
-    start_table();
-    echo "
-        <tr>
-          <th>".tr(SCOSC_PROJECTNAME)."</th>
-          <th>".tr(SCOSP_THEPROJECT)."</th>
-          <th>".tr(SCOSC_SOURCES)."</th>
-          <th>".tr(SCOSC_TOOLS)."</th>
-        </tr>
+}
+$result = mysql_query("SELECT * FROM scos_project WHERE id > 1");
+
+if (mysql_numrows($result) > 0) {
+    if ($xml) {
+        echo "<scos_projects>\n";
+    } else {
+	start_table();
+	echo "
+	    <tr>
+	      <th>".tr(SCOSC_PROJECTNAME)."</th>
+	      <th>".tr(SCOSP_THEPROJECT)."</th>
+	      <th>".tr(SCOSC_SOURCES)."</th>
+	      <th>".tr(SCOSC_TOOLS)."</th>
+	    </tr>
+	";
+    }
+    while ($proj = mysql_fetch_object($result)) {
+	$friendly_name = $proj->user_friendly_name;
+	if (!$friendly_name) {
+	    $friendly_name = "?";
+	}
+	if ($xml) {
+	    echo "  <scos_project>\n";
+	    echo "    <id>$proj->id</id>\n";
+	    echo "    <active>$proj->active</active>\n";
+	    echo "    <name>$proj->name</name>\n";
+	} else {
+	    echo "
+		<tr>
+		  <td><a name='proj$proj->id'>$proj->name</a>
+	    ";
+	    if ($proj->active) {
+		echo tr(SCOSC_ACTIVE);
+	    } else {
+		echo tr(SCOSC_NOT_ACTIVE);
+	    }
+	    echo "
+		  </td>
+		  <td>$friendly_name</td>
+	    ";
+
+	    echo "
+		  <td>
+	    ";
+	    $r2 = mysql_query('SELECT * '
+		    . 'FROM scos_source '
+		    . "WHERE project = $proj->id");
+
+	    if (mysql_numrows($r2) > 0) {
+		echo "
+		    <ul>
+		";
+		while ($av = mysql_fetch_object($r2)) {
+		    echo "<li>";
+		    if ($av->type == 1) {
+			echo "<a href='subversionsrc.php?source=$av->id'>";
+			echo tr(SCOSC_SVN_SOURCE_URL);
+			echo " $av->url</a>";
+		    } else {
+			echo "".tr(SCOSC_UNKNOWN_TYPE)."";
+		    }
+		    echo "</li>";
+		}
+		echo "
+		    </ul>
+		";
+	    } else {
+		echo tr(SCOSC_NO_SOURCE);
+	    }
+	    echo "
+		  </td>
+	    ";
+
+	    echo "
+		  <td>
+	    ";
+	    $r3 = mysql_query('SELECT * '
+		    . 'FROM scos_tool '
+		    . "WHERE project = $proj->id "
+		    . 'AND project > 1');
+
+	    if (mysql_numrows($r3) > 0) {
+		echo "
+		    <ul>
+		";
+		while ($av = mysql_fetch_object($r3)) {
+		    echo "<li>";
+		    echo "<a href='configtools.php?project=$proj->id'>
+			".tr(SCOSC_CONFIG_TOOLS)."</a>
+		    ";
+		    echo " $av->name";
+		    echo " $av->config ";
+		    if ($av->active) {
+			echo tr(SCOSC_ACTIVE);
+		    } else {
+			echo tr(SCOSC_NOT_ACTIVE);
+		    }
+		    echo "</li>";
+		}
+		echo "
+		    </ul>
+		";
+	    } else {
+		echo "<a href='configtools.php?project=$proj->id'>"
+		    .tr(SCOSC_NO_TOOL)."</a>
+		";
+	    }
+	    echo "
+		  </td>
+	    ";
+
+	    echo "
+		</tr>
+	    ";
+	}
+
+	if ($xml) {
+	    echo "  </scos_project>\n";
+	} else {
+	    echo "
+		</tr>
+	    ";
+	}
+    }
+    mysql_free_result($result);
+
+    if ($xml) {
+	echo "</scos_projects>\n";
+    } else {
+	end_table();
+    }
+} else {
+    echo tr(SCOSC_NO_PROJECTS)."<p>
     ";
 }
-$result = mysql_query("select * from scos_project");
-
-while ($proj = mysql_fetch_object($result)) {
-    $friendly_name = $proj->user_friendly_name;
-    if (!$friendly_name) {
-	$friendly_name = "?";
-    }
-    if ($xml) {
-        echo "  <scos_project>\n";
-        echo "    <id>$proj->id</id>\n";
-	echo "    <active>$proj->active</active>\n";
-        echo "    <name>$proj->name</name>\n";
-    } else {
-        echo "
-            <tr>
-              <td><a name='$proj->name'>$proj->name</a>
-	";
-	if ($proj->active) {
-	    echo tr(SCOSC_ACTIVE);
-	} else {
-	    echo tr(SCOSC_NOT_ACTIVE);
-	}
-	echo "
-	      </td>
-              <td>$friendly_name</td>
-        ";
-
-	echo "
-	      <td>
-	";
-	$r2 = mysql_query('SELECT * '
-		. 'FROM scos_source '
-		. "WHERE project = $proj->id");
-
-	if (mysql_numrows($r2) > 0) {
-	    echo "
-		<ul>
-	    ";
-	    while ($av = mysql_fetch_object($r2)) {
-		echo "<li>";
-		if ($av->type == 1) {
-		    echo "<a href='subversionsrc.php?source=$av->id'>";
-		    echo tr(SCOSC_SVN_SOURCE_URL);
-		    echo " $av->url</a>";
-		} else {
-		    echo "".tr(SCOSC_UNKNOWN_TYPE)."";
-		}
-		echo "</li>";
-	    }
-	    echo "
-		</ul>
-	    ";
-	} else {
-	    echo tr(SCOSC_NO_SOURCE);
-	}
-        echo "
-              </td>
-        ";
-
-	echo "
-	      <td>
-	";
-	$r3 = mysql_query('SELECT * '
-		. 'FROM scos_tool '
-		. "WHERE project = $proj->id");
-
-	if (mysql_numrows($r3) > 0) {
-	    echo "
-		<ul>
-	    ";
-	    while ($av = mysql_fetch_object($r3)) {
-		echo "<li>";
-		echo "<a href='projtool.php?tool=$av->id'>";
-		echo " $av->name</a>";
-		echo " $av->config ";
-		if ($av->active) {
-		    echo tr(SCOSC_ACTIVE);
-		} else {
-		    echo tr(SCOSC_NOT_ACTIVE);
-		}
-		echo "</li>";
-	    }
-	    echo "
-		</ul>
-	    ";
-	} else {
-	    echo tr(SCOSC_NO_TOOL);
-	}
-        echo "
-              </td>
-        ";
-
-        echo "
-            </tr>
-        ";
-    }
-
-    if ($xml) {
-        echo "  </scos_project>\n";
-    } else {
-        echo "
-            </tr>
-        ";
-    }
-}
-mysql_free_result($result);
 
 if ($xml) {
-    echo "</scos_projects>\n";
 } else {
-    end_table();
     echo tr(SCOSC_PROJECT_HELP)."<p>
     ";
     page_tail();
